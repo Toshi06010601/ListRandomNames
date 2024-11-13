ca83136579b5e0ff8fe8525d178fad9195eeba6f
Toshinobu Nakamura <nkmr104@outlook.com>
2024-11-13 18:07:57 +0000

    Completed 2

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const $button = document.getElementById("addBtn");
 const $lists = document.getElementById("lists");
 
+//DOM
+function addList(user) {
+    const list = document.createElement("li");
+    list.innerText = user.name;
+    $lists.appendChild(list);
+}
+
+//関数（メソッド）
 async function getUsers() {
-    //データのやりとり
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
     return users;
 }
-
-
-
+//関数（メソッド）
 async function listUsers() {
-    const users = getUsers();
-    users.forEach(user => {
-        //DOM操作
-        const list = document.createElement("li");
-        list.innerText = user.name;
-        $lists.appendChild(list);
-    });
+    const users = await getUsers();
+    users.forEach(addList);
 }
 
-$button.addEventListener("click", listUsers);
-
+//イベント
 window.addEventListener("load", listUsers);
+$button.addEventListener("click", listUsers);
